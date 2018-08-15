2233d77ba8e94033ec856bf0d4ef386955432b81
Franco Giannotti <francogiannotti115@gmail.com>
2018-08-15 02:19:28 -0300

    agrego endpoint y funcion DeleteUser

--- controller/user.go
+++ controller/user.go
@@ -20,3 +20,23 @@ func Post(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, gin.H{"status": "success", "message": "El usuario fue creado"})
 }
+func DeleteUser(c *gin.Context) {
+	// dar error si el user id no es valido
+	var u domain.User
+
+	id := c.Param("user_id")
+	if id == "" { // solo me molesta si no hay nada, porque en ese caso gorm decide borrar todo
+		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "message": "El usuario es invalido"})
+		return
+	}
+
+	err := db.Where("user_id = ?", id).First(&u).Error
+	if err != nil {
+		//error handling?
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "El usuario no ha sido encontrado"})
+	}
+	//borrar de la db
+	db.First(&u, "user_id = ?", id) // esto es para que u reciba la info y hacer delete (podria hacerlo solo una vez antes de err)
+	db.Delete(&u)                   //puede fallar al borrar?
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "El usuario ha sido borrado"})
+}
